c50cd77b608091d2421b5c2e252cc16ecf623a2e
owoadehabeeb04 <owoadehabeeb04@gmail.com>
2023-07-08 04:27:38 +0100

    fixed bugs

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
+"strict mode";
+// hamburgermenu
+const hamburger = document.querySelector('.hamburger')
+const list = document.querySelector('.navlink ul')
+const close = document.querySelector('.close')
+console.log(list);
+console.log(
+hamburger.addEventListener('click', function(){
+close.classList.remove('hidden')
+list.classList.add('listed')
+hamburger.style.opacity = '0'
+}))
+close.addEventListener('click', function(){
+    list.classList.remove('listed')
+    close.classList.add('hidden') 
+    hamburger.style.opacity = '1'
+})
 
+// Activenav
+
+const nav_items = document.querySelectorAll('.nav_link_item')
+
+nav_items.forEach((navs)=>{
+    navs.addEventListener('click', function(){
+        list.classList.remove('listed')
+        close.classList.add('hidden') 
+        hamburger.style.opacity = '1'
+        nav_items.forEach((navs)=>{
+            navs.classList.remove('activeNav')
+        })
+        navs.classList.add('activeNav')
+    })
+})
 
